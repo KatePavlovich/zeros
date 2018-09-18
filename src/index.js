29c36202fb8cf790d23b75d0c141622154b75b83
KatePavlovich <asylumhere@mail.ru>
2018-09-18 17:04:43 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+  let arr = [];
+  let divisor = 5;
+
+  while (divisor <= number) {
+    arr.push(parseInt(number / divisor));
+    divisor *= 5;
+  }
+
+  return arr.reduce((p, c) => p + c, 0);
 }
